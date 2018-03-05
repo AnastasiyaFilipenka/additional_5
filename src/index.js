8cadf684a1479b70f974a51c03d34883c1e61f77
Anastasiya <nfilipenk@yandex.ru>
2018-03-05 18:28:06 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  
+		  
+
+  
+	    for (var i=0; i<bracketsConfig.length; i++)
+		for(var j=0;j<bracketsConfig.length;j++){
+  var subAr=bracketsConfig[i][j];
+  }
+  var str='';
+  var el=str.split('');
+  
+    	
+	  if(el.length%2!=0 && el!=subAr){
+	  return 0;
+	  }else{
+	  return 1;}
+	 
+	  
+  
+	  
+  
 }
+  
+	  
+  
+
